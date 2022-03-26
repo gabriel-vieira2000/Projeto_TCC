2ab60a8f5ea2a01a8a4cd12a9fcb5cec6ba6f9ea
Gabriel Vieira Cardoso <gvc20002016@gmail.com>
2022-03-26 14:06:45 -0300

    Criando um App Django para o Site Web do Projeto

---
+++ site_controle_estoque_medicamentos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SiteControleEstoqueMedicamentosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'site_controle_estoque_medicamentos'
